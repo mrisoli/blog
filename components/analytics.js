52eb6dd60d0e5a16e857c82e66f45ba7aa7caf67
Marcelo Risoli <celorisoli@gmail.com>
2020-09-02 19:22:57 +0200

    fix analytics code

--- components/analytics.js
+++ components/analytics.js
@@ -1,19 +1,20 @@
 const Analytics = () => (
-  <script
-    dangerouslySetInnerHTML={{
-      __html:`
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-86504769-1"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag() {
-            dataLayer.push(arguments)
-          }
-          gtag('js', new Date());
+  <>
+    <script
+      async
+      src="https://www.googletagmanager.com/gtag/js?id=UA-86504769-1"
+    />
 
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
           gtag('config', 'UA-86504769-1');
-        </script>
-      `
-    }}
-  />
+        `
+      }}
+    />
+ </>
 )
 export default Analytics
