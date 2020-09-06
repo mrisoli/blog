877d3ce2c99f3967a0ca72d6f8ca515c309d11e0
Marcelo Risoli <celorisoli@gmail.com>
2020-09-06 15:45:38 +0200

    post setup

---
+++ pages/2020/an-unlikely-update.js
@@ -0,0 +1,15 @@
+import Post from '../../components/layouts/post'
+import Header from '../../components/post/header'
+import P from '../../components/post/paragraph'
+
+const Article = () => (
+  <Post>
+    <Header title="An Unlikely Update" date="September 6, 2020" />
+
+    <P>
+      Start
+    </P>
+  </Post>
+)
+
+export default Article
