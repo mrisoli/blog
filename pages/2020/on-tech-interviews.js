741cde5a980884180a45079633499dc5a1e5a115
Marcelo Risoli <celorisoli@gmail.com>
2020-09-23 22:29:36 +0200

    Post 5 setup

---
+++ pages/2020/on-tech-interviews.js
@@ -0,0 +1,14 @@
+import Post from '../../components/layouts/post'
+import Header from '../../components/post/header'
+import H2 from '../../components/post/heading'
+import P from '../../components/post/paragraph'
+
+const Article = () => (
+  <Post>
+    <Header title="On tech interviews" date="September 23, 2020" />
+    <H2 />
+    <P />
+  </Post>
+)
+
+export default Article
