a0b5f9fdb43159092fbcff8d2fa22771589e361c
Marcelo Risoli <celorisoli@gmail.com>
2020-11-28 17:57:59 -0300

    add og and twitter meta tags (#20)
    * add og and twitter meta tags
    
    * increase image size

--- pages/_app.js
+++ pages/_app.js
@@ -31,6 +31,19 @@ export default function MyApp({ Component, pageProps }) {
         <meta name="description" content="Personal Blog by Marcelo Risoli. I talk tech" />
         <meta name="keywords" content="Blog, Tech, Marcelo Risoli" />
         <meta name="theme-color" content="#000000" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://risoli.dev/" />
+        <meta property="og:title" content="Marcelo Risoli Blog" />
+        <meta property="og:description" content="Personal Blog by Marcelo Risoli. I talk tech." />
+        <meta property="og:image" content="https://en.gravatar.com/userimage/52005801/f8d14e95204e2d58667419f1d3a9b29c.jpg?size=2048" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://twitter.com/mgrisoli" />
+        <meta property="twitter:title" content="Marcelo Risoli Blog" />
+        <meta property="twitter:description" content="Personal Blog by Marcelo Risoli. I talk tech." />
+        <meta property="twitter:image" content="https://en.gravatar.com/userimage/52005801/f8d14e95204e2d58667419f1d3a9b29c.jpg?size=2048" />
+
         <script
           data-name="BMC-Widget"
           src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
