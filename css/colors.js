bffcf69c7398d4505249e7b8a95301359cbc8583
Marcelo Risoli <celorisoli@gmail.com>
2020-12-17 17:16:40 -0300

    move all colors to color variables

--- css/colors.js
+++ css/colors.js
@@ -5,6 +5,14 @@ const style = css.global`
     --link-color: #0c00ff;
     --link-highlight: yellow;
     --meta-text-color: #4f4f4f;
+    --ui-item-color: #5b5b5b;
+    --ui-item-color-hover: #eee;
+    --ui-item-color-active: #ccc;
+    --nav-link-color: #333;
+    --nav-button-text--color: #fff;
+    --nav-button-background--color: #000;
+    --logo-text--color: #000;
+    --logo-background-color-active: #fff445;
   }
   ::selection {
     background-color: #0070f3;
