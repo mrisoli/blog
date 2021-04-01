c69e97ee616752286b5a245552f3679d86ae9892
Marcelo Risoli <celorisoli@gmail.com>
2021-04-01 10:52:45 +0200

    switch prezi for infogram

--- components/prezi.js
+++ components/prezi.js
@@ -1,11 +1,20 @@
+import { useEffect} from 'react'
+function graphicFn(e,i,n,s){var t="InfogramEmbeds",d=e.getElementsByTagName("script")[0];if(window[t]&&window[t].initialized)window[t].process&&window[t].process();else if(!e.getElementById(n)){var o=e.createElement("script");o.async=1,o.id=n,o.src="https://e.infogram.com/js/dist/embed-loader-min.js",d.parentNode.insertBefore(o,d)}};
+
 const PreziDesign  = () => {
+  useEffect(() => {
+    if (document) {
+      graphicFn(document,0,"infogram-async")
+    }
+  }, [])
+
   return (
     <div className="container">
-      <script async defer src="https://e.prezicdn.net/v1/design.js"></script><div className="prezi-design-embed" data-project-id="yzdulug3s3qw"></div>
+      <div className="infogram-embed" data-id="27ddf7a2-b784-4919-93e4-fb3b9238a240" data-type="interactive" data-title="Hive"></div>
       <style jsx>{`
-        .prezi-design-embed {
-          height: 20em;
-          width: 36em;
+        .container {
+          height: 25em;
+          width: 38em;
         }
       `}</style>
     </div>
