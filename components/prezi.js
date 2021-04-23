5955fb649c8adf4736974bfa4b06e2cc4d830bc7
Marcelo Risoli <celorisoli@gmail.com>
2021-04-23 23:37:45 +0200

    add hive sprint post

--- components/prezi.js
+++ components/prezi.js
@@ -1,4 +1,5 @@
 import { useEffect} from 'react'
+
 function graphicFn(e,i,n,s){var t="InfogramEmbeds",d=e.getElementsByTagName("script")[0];if(window[t]&&window[t].initialized)window[t].process&&window[t].process();else if(!e.getElementById(n)){var o=e.createElement("script");o.async=1,o.id=n,o.src="https://e.infogram.com/js/dist/embed-loader-min.js",d.parentNode.insertBefore(o,d)}};
 
 const PreziDesign  = () => {
